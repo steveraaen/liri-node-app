aba4aefecad8c86470b6e05611f898fd0f00e49c
steveraaen <sraaen@gmail.com>
2017-02-13 16:44:46 -0500

    I deserve a D on this

--- keys.js
+++ keys.js
@@ -1,9 +1,8 @@
-const keys = require('./liri.js')
-exports.twitterKeys = {
-  consumer_key:'0NnRfQzKbUqICsAnXmEPGHfry',
-  consumer_secret: 'TXBVryflFhY1olcSvpqBXbBwnPBgcDnwwYiVhYhwVq9t4cqpSl',
+console.log('this is loaded');
+
+module.exports = {
+  consumer_key: '8QzyJ9Ngb568DEBc5nv51TBiJ',
+  consumer_secret: 'EqaDtyA3ydCUV8zQtUSMncZzHJlzczznL574AB2XcCbELucPK9',
   access_token_key: '11233182-pcsuP3N9IzWDIlNvml12YKIOYh5QoajqcNVVF1obG',
   access_token_secret: 'rSWx5BO9A1dmun0Vf0EGSe6CjwismdIB4qNV7fROEO9Kt'
 }
-
-
--- liri.js
+++ liri.js
@@ -1,30 +1,40 @@
+'use strict'
 // Get required packages
-const fs = require('fs')
-    /*const keys = require('./keys.js')*/
-const request = require('request');
+const keys = require('./keys.js')
 const Twitter = require('twitter');
+const fs = require('fs')   
+const request = require('request');
 const spotify = require('spotify');
 //  Inital variables
 var twtr;
-var searchVal = process.argv[3];
+var searchVal = "mr nobody";
+
 var lookup = process.argv[2];
 // Twitter credntials (SHOULD BE IN keys.js )
 var client = new Twitter({
-    consumer_key: '0NnRfQzKbUqICsAnXmEPGHfry',
-    consumer_secret: 'TXBVryflFhY1olcSvpqBXbBwnPBgcDnwwYiVhYhwVq9t4cqpSl',
-    access_token_key: '11233182-pcsuP3N9IzWDIlNvml12YKIOYh5QoajqcNVVF1obG',
-    access_token_secret: 'rSWx5BO9A1dmun0Vf0EGSe6CjwismdIB4qNV7fROEO9Kt'
+    consumer_key : keys.consumer_key,
+    consumer_secret : keys.consumer_secret,
+    access_token_key : keys.access_token_key,
+    access_token_secret : keys.access_token_secret
 });
 
 if (lookup === 'my-tweets') {
-    client.get('search/tweets', { q: 'SteveRaaen' }, function(error, tweets, response) {
-        console.log(tweets.statuses[0].created_at);
-        console.log(tweets.statuses[0].text);
-    });
+    searchVal = process.argv[3];
+    client.get('search/tweets', { q: 'SteveRaaen' }, function(err, tweets, response) {
+            if ( err ) {
+        
+        return;
+    } else for (let i = 0; i < tweets.statuses.length; i++){
+/*        console.log(tweets.statuses[0].created_at);
+        console.log(tweets.statuses[0].text);*/
+
+        console.log('At  - ' + tweets.statuses[i].created_at + '<br>Steve Tweeted  :' + tweets.statuses[i].text);
+   } })
 } else if (lookup === 'spotify-this-song') {
+    searchVal = process.argv[3];
 spotify.search({ type: 'track', query: searchVal}, function(err, data) {
     if ( err ) {
-        console.log('Error occurred: ' + err);
+        console.log('Error occurred: ' + err[0]);
         return;
     }
  	for (var i = 0; i < data.tracks.items.length; i++) {
@@ -34,14 +44,35 @@ spotify.search({ type: 'track', query: searchVal}, function(err, data) {
     console.log('Preview URL =  ' + data.tracks.items[i].preview_url);
 }
 });
+
 } else if (lookup === 'movie-this') {
-	request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&r=json", function(error, response, body) {
+searchVal = process.argv[3];
+	request("http://www.omdbapi.com/?t=" + searchVal + "&plot=short&r=json", function(error, response, body) {
 		if (error){console.log('error')}
   // If there were no errors and the response code was 200 (i.e. the request was successful)...
   if (!error && response.statusCode === 200) {
-
+console.log(searchVal)
     // Then we print out the imdbRating
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    console.log("The movie's title is : " + JSON.parse(body).Title);
+    console.log("The year it was released: " + JSON.parse(body).Year);
+    console.log("The movie's IMDB rating: " + JSON.parse(body).imdbRating);
+    console.log("The movie's language is: " + JSON.parse(body).Language);
+    console.log("The movie was made in: " + JSON.parse(body).Country);
+    console.log("The following actors appeared: " + JSON.parse(body).Actors);
+    console.log("The movie's is about: " + JSON.parse(body).Plot);
+    console.log("Rotten Tomato : " + JSON.parse(body).tomatoUserRating);
+
+        console.log(body);
   }
+
 });
+}else if (lookup === 'do-what-it-says'){
+    var dwis = fs.readFile('random.txt', 'utf8', function(err, data){
+        if(err){throw err}
+            data = data.split(',')
+            lookup = data[0];
+            searchVal = data[1]
+            console.log(data)
+    })
+    
 }
